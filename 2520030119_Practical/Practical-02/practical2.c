ba2dd6d719da366ba7dd70556cfd1852fd780e12
Shreyaskura <Shreyaskura@gmail.com>
2026-08-24 05:27:05 +0000

    Add Practical 02 file copy and system call tracing

---
+++ 2520030119_Practical/Practical-02/practical2.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main() {
+    int source_fd, destination_fd;
+    char buffer[1024];
+    ssize_t bytes_read, bytes_written;
+
+    source_fd = open("source.txt", O_RDONLY);
+
+    if (source_fd < 0) {
+        perror("Error opening source file");
+        return 1;
+    }
+
+    destination_fd = open("destination.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+    if (destination_fd < 0) {
+        perror("Error opening destination file");
+        close(source_fd);
+        return 1;
+    }
+
+    while ((bytes_read = read(source_fd, buffer, sizeof(buffer))) > 0) {
+        bytes_written = write(destination_fd, buffer, bytes_read);
+
+        if (bytes_written != bytes_read) {
+            perror("Error writing to destination file");
+            close(source_fd);
+            close(destination_fd);
+            return 1;
+        }
+    }
+
+    if (bytes_read < 0) {
+        perror("Error reading source file");
+    }
+
+    close(source_fd);
+    close(destination_fd);
+
+    printf("File copied successfully.\n");
+
+    return 0;
+}
+
